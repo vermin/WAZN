f18f8e96321863cbeb64a3796f7ea501c879549b
vermin <vermin@techie.com>
2021-03-20 01:54:49 +0100

    1.2.8

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -5379,7 +5379,7 @@ void Blockchain::cancel()
 }
 
 #if defined(PER_BLOCK_CHECKPOINT)
-static const char expected_block_hashes_hash[] = "DF3F619804A92FDB4057192DC43DD748EA778ADC52BC498CE80524C014B81119";
+static const char expected_block_hashes_hash[] = "F3CF190517B9323D27CA15830222D9958176849EE36D1D1AD64AF7509B2D855A";
 void Blockchain::load_compiled_in_block_hashes(const GetCheckpointsCallback& get_checkpoints)
 {
   if (get_checkpoints == nullptr || !m_fast_sync)
